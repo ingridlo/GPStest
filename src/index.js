05c28ef35431261ee3a3fec8b3f39c59794953a8
ingridlo <ingridloargote@gmail.com>
2023-02-03 10:56:11 -0500

    first commi

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GPS from "gps"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const gps = new GPS();
+gps.on('data', parsed => {
+  console.log(parsed);
+});
+gps.update("$GPGGA,224900.000,4832.3762,N,00903.5393,E,1,04,7.8,498.6,M,48.0,M,,0000*5E");
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
