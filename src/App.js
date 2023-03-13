51068204800900f945c676562725bfc983fe62b7
ingridlo <ingridloargote@gmail.com>
2023-03-13 18:37:37 -0500

    precision

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [position, setPosition] = useState(null);
+  const [accuracy, setAccuracy] = useState(null);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -12,20 +12,12 @@ function App() {
   }, []);
 
   function showLocation(position) {
-    setPosition(position.coords.accuracy);
-    //console.log(`Actualización de posición recibida: lat=${lat}, lon=${lon}, altitude=${altitude}`);
-    //const timestamp = new Date(position.timestamp).toISOString().replace(/\..+/, '');
-    //const nmea = `$GPGGA,${timestamp},${lat.toFixed(4)},N,${lon.toFixed(4)},W,1,08,1.0,${altitude.toFixed(1)},M,,-33.7,M,,`;
-    //gps.update(nmea);
+    setAccuracy(position.coords.accuracy);
   }
 
   return (
     <div>
-      {position ? (
-        <p>Precision: {position}</p>
-      ) : (
-        <p>Retrieving geolocation data...</p>
-      )}
+      {accuracy && <p>Precision: {accuracy}</p>}
     </div>
   );
 }
